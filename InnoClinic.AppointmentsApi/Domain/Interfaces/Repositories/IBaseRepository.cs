ac0729b45bc1c758d6fa28cbcce1473371ecb095
Egor Semenov <egor123semenov@gmail.com>
2023-12-05 17:52:29 +0300

    NIS-9422: Implement Part Of AppointmentsApi

---
+++ InnoClinic.AppointmentsApi/Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        IQueryable<T> FindAll(bool isTrackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool isTrackChanges);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
